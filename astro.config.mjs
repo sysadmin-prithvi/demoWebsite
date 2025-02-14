f037729f8de68857f85094811ac24b997ba3c7ac
snehatumminakatti1 <snehatumminakatti1@gmail.com>
2025-02-14 14:14:11 +0530

    changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://sysadmin-prithvi.github.io/demoWebsite/",
-    output: 'static',
-    outDir: './docs',
-    build: {
-      assets: 'astro'
-    }
+    
 });
